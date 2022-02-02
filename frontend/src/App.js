0348c23313be7b8fcbf30439b21e9798b8d0db14
abhinav prajapati <email>
2022-02-02 20:32:55 +0530

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Post } from "./Post";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app_header">
+        <img className="app_header-image"  src="./instaname.png" alt="" />
+      </div>
+      <h1>ABHINAV</h1>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      {/* Header */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default App;
